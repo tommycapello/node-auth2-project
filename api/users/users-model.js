a30b38297ba300e69b6d92411e70141be2c0aef0
tommycapello <thomcapello@gmail.com>
2021-07-20 18:26:29 -0400

    wip

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -18,6 +18,10 @@ function find() {
       }
     ]
    */
+
+    return db('users')
+          .join('roles', 'users.role_id', 'roles.role_id')
+          .select('user_id', 'username', 'role_name')
 }
 
 function findBy(filter) {
@@ -34,6 +38,10 @@ function findBy(filter) {
       }
     ]
    */
+    db('users')
+    .join('roles', 'users.role_id', 'roles.role_id')
+    .select('user_id', 'username', 'password', 'role_name')
+    .where(filter)
 }
 
 function findById(user_id) {
@@ -47,6 +55,11 @@ function findById(user_id) {
       "role_name": "instructor"
     }
    */
+    return db('users')
+    .join('roles', 'users.role_id', 'roles.role_id')
+    .select('user_id', 'username', 'role_name')
+    .where('users.user_id', user_id).first()
+
 }
 
 /**
